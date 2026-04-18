e784bbb2248c9a3f3498a097a2543922187c4ffb
Ebube Williams <chidiekwebelam@gmail.com>
2026-04-18 09:48:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: "Ebube",
+      lastName: "Ezeh",
+      url: "https://google.com",
+      age: 20,
+    };
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
+    },
+    updateLastName(event) {
+      this.lastName = event.target.value;
+    },
+  },
+}).mount("#app");
+
+// setTimeout(() => {
+//   vm.firstName = "Dike";
+// }, 2000);
+
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: "dike",
+//       lastName: "Ezeh",
+//     };
+//   },
+// }).mount("#app2");
