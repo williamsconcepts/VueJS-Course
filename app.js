305aa994c4c8ab14a240e3f66e860e32e73a5b0e
Ebube Williams <chidiekwebelam@gmail.com>
2026-04-20 16:17:09 +0100

    Computed Properties

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ const vm = Vue.createApp({
     };
   },
   methods: {
-    fullName() {
-      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`;
-    },
     increment() {
       this.age++;
     },
     updateLastName(msg, event) {
-      console.log(msg);
+      // console.log(msg);
 
       this.lastName = event.target.value;
     },
     updateMiddleName(event) {
       this.middleName = event.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      console.log("Full name computed property was called");
+      this.age;
+      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`;
     },
   },
 }).mount("#app");
