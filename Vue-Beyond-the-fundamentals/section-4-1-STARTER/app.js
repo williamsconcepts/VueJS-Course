72e57e87dcf61fbe98305abef0f4441b0a524a57
williamsconcepts <chidiekwebelam@gmail.com>
2026-05-03 17:35:34 +0100

    The Vue Compiler

--- Vue-Beyond-the-fundamentals/section-4-1-STARTER/app.js
+++ Vue-Beyond-the-fundamentals/section-4-1-STARTER/app.js
@@ -4,34 +4,21 @@ let vm = Vue.createApp({
       message: "Hello world!"
     }
   },
-  beforeCreate() {
-    console.log('beforeCreate() function called!', this.message)
-  },
-  created() {
-    console.log('created() function called!', this.message)
-  },
-  beforeMount() {
-    console.log('beforeMount() function called!', this.$el)
-  },
-  mounted() {
-    console.log('mounted() function called!', this.$el)
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate() function called!')
-  },
-  updated() {
-    console.log('update() function called!')
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount() function called!')
-  },
-  unmounted() {
-    console.log('unmounted() function called!')
-  }
+ // template: ``
 })
 
 vm.mount('#app')
 
-// setTimeout(() => {
-//   vm.mount('#app')
-// }, 3000)
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!"
+//     }
+//   },
+//   render() {
+//     return Vue.h(
+//       'h1',
+//       this.message
+//     )
+//   }
+// }).mount("#app2")
